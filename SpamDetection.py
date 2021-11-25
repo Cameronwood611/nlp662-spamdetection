8f721ea272a204775f0936679b15c90dd073b34a
Cameronwood611 <cwood611@uab.edu>
2021-11-25 12:35:35 -0600

    Add deps

---
+++ SpamDetection.py
@@ -0,0 +1,9 @@
+
+# STL
+import re
+
+# PDM
+import numpy as np
+import tensorflow as tf
+from sklearn.metrics import f1_score, recall_score, precision_score, confusion_matrix
+from sklearn.model_selection import train_test_split
